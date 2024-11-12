bb8410483c64bb073f11f7e0b62873a16e48b4a0
Emma <yselaerrm@gmail.com>
2024-11-12 03:26:40 -0500

    Add routes and shared components

---
+++ src/app/pages/user/user-goals/user-goals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-goals',
+  templateUrl: './user-goals.component.html',
+  styleUrls: ['./user-goals.component.css']
+})
+export class UserGoalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/user/user-reminders/user-reminders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-reminders',
+  templateUrl: './user-reminders.component.html',
+  styleUrls: ['./user-reminders.component.css']
+})
+export class UserRemindersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
