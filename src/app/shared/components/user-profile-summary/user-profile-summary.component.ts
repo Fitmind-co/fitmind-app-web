6b808427d6b21685aa0e8877fbd2f09659a00662
Emma <yselaerrm@gmail.com>
2024-11-13 09:10:37 -0500

    Dashboard implementation

---
+++ src/app/shared/components/user-profile-summary/user-profile-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-summary',
+  templateUrl: './user-profile-summary.component.html',
+  styleUrls: ['./user-profile-summary.component.css']
+})
+export class UserProfileSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
