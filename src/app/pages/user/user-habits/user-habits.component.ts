b065ab08d88d5ec71cdaf483eb8dd96fb3d23052
Ale <145732277+alefs9@users.noreply.github.com>
2024-11-20 13:36:36 -0500

    lista de ejercicios

--- src/app/pages/user/user-habits/user-habits.component.ts
+++ src/app/pages/user/user-habits/user-habits.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-user-habits',
+  selector: 'app-user-habits', 
+  standalone: true,
   templateUrl: './user-habits.component.html',
-  styleUrls: ['./user-habits.component.css']
+  styleUrls: ['./user-habits.component.css'],
+  imports: [CommonModule]
 })
-export class UserHabitsComponent implements OnInit {
+export class UserHabitComponent {
+  user = {
+    name: 'Sofía Ramírez',
+    age: 28,
+    height: '160 cm',
+    weight: '60 kg',
+    gender: 'Femenino',
+    profileImage: 'assets/profile.jpg'
+  };
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  habits = [
+    {
+      title: 'Meditación Diaria',
+      duration: '10 minutos',
+      frequency: 'Diaria',
+      time: '7:00 am',
+      startDate: '01 de nov. 2024',
+      description: 'Practicar meditación cada mañana para mejorar la concentración y reducir el estrés.',
+      streak: 3,
+      progress: ['missed', 'completed', 'completed', 'completed', 'missed', 'missed', 'missed'] // Definir los estados de progreso
+    },
+    {
+      title: 'Lectura Diaria',
+      duration: '10 minutos',
+      frequency: 'Diaria',
+      time: '8:00 pm',
+      startDate: '20 de oct. 2024',
+      description: 'Leer todos los días para adquirir nuevos conocimientos y reducir el estrés.',
+      streak: 20,
+      progress: ['completed', 'completed', 'completed', 'completed', 'completed', 'completed', 'completed'] // Definir los estados de progreso
+    }
+  ];
 }
--- src/app/pages/user/user.routes.ts
+++ src/app/pages/user/user.routes.ts
@@ -2,7 +2,6 @@ import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserHabitsComponent } from './user-habits/user-habits.component';
 
 export const userRoutes: Routes = [
   {
@@ -11,9 +10,10 @@ export const userRoutes: Routes = [
     children: [
       {path: 'profile', component: UserProfileComponent},
       {path: 'settings', component: UserSettingsComponent},
-      {path: 'habits', component: UserHabitsComponent},
       {path: ' ', redirectTo: 'profile', pathMatch: 'full'},
 
     ]
   }
 ];
+
+
